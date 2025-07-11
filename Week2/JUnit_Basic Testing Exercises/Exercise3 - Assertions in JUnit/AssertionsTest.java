6fa6868a6c46af1e067ec24369fba12587f9a03a
YogashreeNithi-04 <yogashreenithik@gmail.com>
2025-07-11 10:37:46 +0530

    Add files via upload

---
+++ Week2/JUnit_Basic Testing Exercises/Exercise3 - Assertions in JUnit/AssertionsTest.java
@@ -0,0 +1,32 @@
+package com.example.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AssertionsTest {
+
+    @Test
+    public void testAssertions() {
+        
+        assertEquals(5, 2 + 3);
+
+       
+        assertTrue(5 > 3);
+
+       
+        assertFalse(5 < 3);
+
+       
+        Object obj1 = null;
+        assertNull(obj1);
+
+        Object obj2 = new Object();
+        assertNotNull(obj2);
+
+        String str = "hello";
+        assertSame(str, str);
+
+       
+        assertNotSame(new String("abc"), new String("abc"));
+    }
+}
