721715a1c9a22aa6f2b2675f67ad23ac978bcbbf
YogashreeNithi-04 <yogashreenithik@gmail.com>
2025-08-09 16:25:46 +0530

    Add files via upload

---
+++ Week6/Exercise5 - cohorttracker/Code/CohortDetails.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './CohortDetails.module.css';
+
+function CohortDetails({ cohort }) {
+  const status = cohort.currentStatus?.toLowerCase().trim();
+
+  const headingStyle = {
+    color: status === 'ongoing' ? 'green' : 'blue'
+  };
+
+  return (
+    <div className={styles.box}>
+      <h3 style={headingStyle}>
+        {cohort.cohortCode} - <span>{cohort.technology}</span>
+      </h3>
+      <dl>
+        <dt>Started On</dt>
+        <dd>{cohort.startDate}</dd>
+        <dt>Current Status</dt>
+        <dd>{cohort.currentStatus}</dd>
+        <dt>Coach</dt>
+        <dd>{cohort.coachName}</dd>
+        <dt>Trainer</dt>
+        <dd>{cohort.trainerName}</dd>
+      </dl>
+    </div>
+  );
+}
+
+export default CohortDetails;
