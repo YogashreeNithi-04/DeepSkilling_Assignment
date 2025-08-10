b10e5724eb88d8cd4b87aa9b15bcdeb3b63acc5d
YogashreeNithi-04 <yogashreenithik@gmail.com>
2025-08-10 22:35:18 +0530

    Add files via upload

---
+++ Week7/Exercise12 - Ticketbookingapp/Code/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <h1>Welcome back</h1>;
+  }
+  return <h1>Please sign up.</h1>;
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogoutClick = () => {
+    setIsLoggedIn(false);
+  };
+
+  let button;
+  if (isLoggedIn) {
+    button = <LogoutButton onClick={handleLogoutClick} />;
+  } else {
+    button = <LoginButton onClick={handleLoginClick} />;
+  }
+
+  return (
+    <div className="App">
+      <Greeting isLoggedIn={isLoggedIn} />
+      {button}
+    </div>
+  );
+}
+
+export default App;
